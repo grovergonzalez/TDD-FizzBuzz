c233bd543c9e9696844e6c5923343f831b1cf47e
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-03-20 20:54:44 -0400

    Verificando si el numero esperado es 3 o multiplo de 3 y mostrando Fizz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,4 +1,8 @@
 function FizzBuzz(num){
+    if(num==3)
+    {
+        return "Fizz";
+    }
     return num;
 };
 
--- src/FizzBuzz.spec.js
+++ src/FizzBuzz.spec.js
@@ -7,8 +7,20 @@ describe("FizzBuzz", () => {
 });
 
 describe("FizzBuzz", () => {
-    it("deberia retornar FizzBuzz", () => {
-      expect(FizzBuzz(3)).toEqual("FizzBuzz");
+    it("deberia retornar 2", () => {
+      expect(FizzBuzz(2)).toEqual(2);
     });
-  });
+});
+
+describe("FizzBuzz", () => {
+    it("deberia retornar Fizz con 3", () => {
+      expect(FizzBuzz(3)).toEqual("Fizz");
+    });
+});
+
+describe("FizzBuzz", () => {
+    it("deberia retornar Fizz con multiplo de 3", () => {
+      expect(FizzBuzz(6)).toEqual("Fizz");
+    });
+});
   
